e817efd2844da15a7aa8df3d2f081eb8695c50e2
MichaelCapo23 <123pie23@gmail.com>
2018-12-17 11:17:42 -0800

    started on problem

---
+++ kaperkar's constant/kap const.js
@@ -0,0 +1,36 @@
+var count = 0;
+
+function getNum() {
+    let number = document.querySelector("#number").value;
+    getConst(number);
+}
+
+// function getConst(num) {
+//     var stringNum = num.toString();
+//     var arrNum = Array.from(stringNum);
+//     var stringNum = arrNum.join("");
+//     var reverseNum = arrNum.reverse();
+//     var newReverse = reverseNum.join("");
+//     var newNum = stringNum - newReverse;
+//     count++;
+//     if( newNum !== 6174 && count < 20) {
+//         console.log("newNum", newNum);
+//         getConst(newNum);
+//     }
+// }
+
+function getConst(number) {
+    var stringNum = number.toString();
+    var arrNumber = Array.from(stringNum);
+    var newArrNumber = arrNumber;
+    var sortedSecond = arrNumber.sort((a,b) => {return a-b});
+    var sortedFirst = sortedSecond.sort((a, b) => {return b- a});
+    sortedSecond = sortedSecond.join("");
+    sortedFirst = sortedFirst.join("");
+    var newNum = sortedFirst - sortedSecond;
+    count++;
+    if(newNum !== 6174 && count < 20) {
+        getConst(newNum)
+    }
+
+}
